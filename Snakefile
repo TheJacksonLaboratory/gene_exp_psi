38490445d821dce9583ada0fb9de0aa02456a499
pnrobinson <peter.robinson@jax.org>
2023-04-11 13:27:24 -0400

    downloads

--- Snakefile
+++ Snakefile
@@ -16,11 +16,19 @@ function error_exit
 
 HTTP_FILES = ["Homo_sapiens.GRCh38.109.gtf",
               "GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_expected_count.gct.gz"]
+
+
+GTEX_FILES = ["GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_expected_count.gct.gz",
+              "GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_tpm.gct.gz",
+              "GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt"]
+
 resources_dir='resources'
 
 rule all:
   input:
-    "after_exon_sig_next.RData"
+    "after_exon_sig_next.RData",
+    expand("resources/{g}", g=GTEX_FILES)
+    #expand("types_{tissue_idx}.RData", tissue_idx = ["1"])
 
 
 rule downloadReference:
@@ -37,23 +45,54 @@ rule downloadReference:
     gunzip Homo_sapiens.GRCh38.109.gtf.gz || error_exit "GTF gunzip failed"
     """
         
-rule downloadGtex:
+rule downloadGtexExpected:
   output:
-      gtex_rsem = resources_dir + '/' +  HTTP_FILES[1]
+    "resources/" + GTEX_FILES[0]
   params:
       cluster_opts='--mem=12G -t 0:20',
       resources_dir = resources_dir
   shell:
       """
-      mkdir -p {params.resources_dir}
-      cd {params.resources_dir}
-      wget https://storage.googleapis.com/gtex_analysis_v8/rna_seq_data/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_expected_count.gct.gz  || error_exit "GTEX/RSEM download failed"
+      mkdir -p {resources_dir}
+      cd {resources_dir}
+      wget https://storage.googleapis.com/gtex_analysis_v8/rna_seq_data/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_expected_count.gct.gz  || error_exit "GTEX/RSEM download failed!"
       """
+
+rule downloadGtexTpm:
+  output:
+    "resources/" + GTEX_FILES[1]
+  params:
+      cluster_opts='--mem=12G -t 0:20',
+      resources_dir = resources_dir
+  shell:
+      """
+      mkdir -p {resources_dir}
+      cd {resources_dir}
+      wget https://storage.googleapis.com/gtex_analysis_v8/rna_seq_data/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_tpm.gct.gz  || error_exit "GTEX/RSEM TPM download failed!"
+      """
+
+
+
+rule downloadGtexSampleAttributes:
+  output:
+    "resources/" + GTEX_FILES[2]
+  params:
+      cluster_opts='--mem=12G -t 0:20',
+      resources_dir = resources_dir
+  shell:
+      """
+      mkdir -p {resources_dir}
+      cd {resources_dir}
+      wget https://storage.googleapis.com/gtex_analysis_v8/annotations/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt  || error_exit "GTEX sample attribs download failed!"
+      """
+
+    
+
     
 rule create_signatures:
   input:
     gtf=resources_dir + '/' + HTTP_FILES[0],
-    gtex=resources_dir + '/' + HTTP_FILES[1]
+    gtex=resources_dir + '/' + GTEX_FILES[0]
   output:
     "after_exon_sig_next.RData"
   params:
@@ -64,3 +103,21 @@ rule create_signatures:
     "====create signatures===="
   shell:
     "Rscript scripts/create_signatures.R {input.gtf} {input.gtex} "
+
+
+rule ge_frac_cor:
+  input:
+    gtf=resources_dir + '/' + HTTP_FILES[0],
+    gtex_transcript_tpm=resources_dir + '/' + GTEX_FILES[1],
+    gtex_sample_attr=resources_dir + '/' + GTEX_FILES[2],
+    after_exon="after_exon_sig_next.RData",
+  output:
+    "types_{tissue_idx}.RData"
+  params:
+    cluster_opts='--mem=64G -t 4:00'
+  conda:
+    "env/gefrac_env.yaml"
+  message:
+    "====ge_frac_cor===="
+  shell:
+    "Rscript scripts/ge_frac_cor.R {input.gtf} {input.gtex_transcript_tpm} {input.gtex_sample_attr} {input.after_exon} {input.tissue_idx} 2"
---
+++ scripts/ge_frac_cor.R
@@ -0,0 +1,126 @@
+#read all gtex
+#convert from transcript counts to exon counts
+#create another matrix with proportions
+#compute correlation between rows of the two matrices
+
+args = commandArgs(trailingOnly=TRUE)
+gtf_file=args[1] # Homo_sapiens.GRCh38.91.gtf
+gtex_tpm_file=args[2] # GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_tpm.gct.gz
+gtex_sample_attributes = args[3] # GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt
+after_exon_sig_next = args[4] # after_exon_sig_next.RData
+tissue_index = as.integer(args[5]) # the index to the tissues array, below
+num.cores = as.integer(args[6]) # e.g., 32
+
+
+
+
+library(data.table)
+
+library(parallel)
+
+
+tissues=c('Lung','Spleen','Thyroid','Brain - Cortex','Adrenal Gland','Breast - Mammary Tissue','Heart - Left Ventricle',
+          'Liver','Pituitary','Pancreas')
+
+load('after_exon_sig_next.RData')
+
+gtf.file=gtf.file[gtf.file$V3=='exon',]
+exon.names=paste(gtf.file$V1,gtf.file$V4,gtf.file$V5,gtf.file$V7,sep='|')
+transcript.ids=gsub(';','',unlist(lapply(strsplit(as.character(gtf.file[,9]),split=' '),'[[',6)))
+transcript.ids=gsub("\"",'',transcript.ids)
+gene.ids=gsub(';','',unlist(lapply(strsplit(as.character(gtf.file[,9]),split=' '),'[[',2)))
+gene.ids=gsub("\"",'',gene.ids)
+
+
+
+gtex.counts=fread(gtex_tpm_file, data.table = F)
+meta.data=read.csv(gtex_sample_attributes, sep='\t', header=T)
+
+meta.data=meta.data[which(meta.data$SMTSD==tissues[tissue.index]),]
+gtex.counts=gtex.counts[,c(1,2,which(colnames(gtex.counts) %in% meta.data$SAMPID))]
+meta.data=meta.data[meta.data$SAMPID %in% colnames(gtex.counts),]
+meta.data=meta.data[match(meta.data$SAMPID,colnames(gtex.counts)[-c(1,2)]),]
+
+sum(meta.data$SAMPID!=colnames(gtex.counts)[-c(1,2)])
+
+gtex.counts$transcript_id=gsub('\\.[0-9]*','',gtex.counts$transcript_id)
+gtex.counts$gene_id=gsub('\\.[0-9]*','',gtex.counts$gene_id)
+
+print(tissues[tissue.index])
+
+exon.proportions=do.call(rbind,lapply(unique(transcript.to.exon[,2]),function(x){
+  
+  tte.row=which(transcript.to.exon[,2]==x)
+
+  genes=transcript.to.exon[tte.row,3]
+  
+  res=do.call(rbind,lapply(unique(genes),function(x){
+    
+    in.transcripts=transcript.to.exon[tte.row[genes==x],1]
+    
+    all.gene.transcripts=gtex.counts$transcript_id[gtex.counts$gene_id==x]
+    
+    colSums(gtex.counts[gtex.counts$transcript_id %in% in.transcripts,-c(1,2)])/
+     colSums(gtex.counts[gtex.counts$transcript_id %in% all.gene.transcripts,-c(1,2)])
+  }))
+  
+  res[is.nan(res) | is.infinite(res)]=NA
+  
+  rownames(res)=paste(unique(genes),x)
+  
+  res
+  
+}))
+
+print('exon.proportions created')
+
+
+
+exon.gene.counts=do.call(rbind,lapply(unique(transcript.to.exon[,2]),function(x){
+  
+  tte.row=which(transcript.to.exon[,2]==x)
+  
+  genes=transcript.to.exon[tte.row,3]
+  
+  res=do.call(rbind,lapply(unique(genes),function(x){
+    
+    all.gene.transcripts=gtex.counts$transcript_id[gtex.counts$gene_id==x]
+    
+    colSums(gtex.counts[gtex.counts$transcript_id %in% all.gene.transcripts,-c(1,2)])
+  }))
+  
+  rownames(res)=paste(unique(genes),x)
+  
+  res[is.nan(res) | is.infinite(res)]=NA
+  
+  res
+  
+}))
+
+
+
+print('exon.gene.counts created')
+
+cor.vals=do.call(rbind,lapply(1:nrow(exon.proportions),function(i){
+  
+  if(sum(is.na(exon.proportions[i,]))>=length(exon.proportions[i,])/2)
+    return(c(NA,NA,NA))
+  
+  if (quantile(exon.gene.counts[i,],0.95)/quantile(exon.gene.counts[i,],0.05)<2)
+    return(c(NA,NA,NA))
+  
+  if (sd(exon.proportions[i,],na.rm=T)==0 || sd(exon.gene.counts[i,],na.rm=T)==0)
+    return(c(NA,NA,NA))
+  
+  res=summary(lm(exon.gene.counts[i,]~exon.proportions[i,],na.action='na.omit'))
+  
+  c(res$coefficients[2,1],res$coefficients[2,4],res$r.squared)
+}))
+
+colnames(cor.vals)=c('coef','pval','r.squared')
+
+save.image(paste0('types_',tissue.index,'.RData'))
+
+
+
+
